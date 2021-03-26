a75a59408fccc9bea4f2ce73316e7a6847492f54
lucas-m-d <68205310+lucas-m-d@users.noreply.github.com>
2021-03-26 17:01:07 +0000

    still something wrong :(

--- sketch.js
+++ sketch.js
@@ -1,4 +1,3 @@
-//import SquareObject from "square"
 const object1 = new SquareObject()
 const object2 = new SquareObject()
 const elasticCollisionSpeed = (m1, m2, v1, v2) => {
@@ -7,10 +6,10 @@ const elasticCollisionSpeed = (m1, m2, v1, v2) => {
   return ({"v1":object1speed, "v2":object2speed})
 }
 const momentumBefore = () => {return(object1.velocity.copy().mult(object1.mass) + object2.velocity.copy().mult(object2.mass))}
-const kineticEnergy = () => {
-  var ke1 = object1.velocity.copy().mult(object1.velocity).mult(0.5).mult(object1.size)
-  var ke2 = object2.velocity.copy().mult(object2.velocity).mult(0.5).mult(object2.size)
-  return (ke1.add(ke2)) 
+const kineticEnergy = () => { 
+  var ke1 = 0.5 * object1.size * object1.velocity.x * object1.velocity.x  
+  var ke2 = 0.5 * object2.size * object2.velocity.x * object2.velocity.x
+  return (ke1 + ke2)
 }
 function setup() {
   createCanvas(400, 400);
@@ -28,31 +27,18 @@ function draw() {
   background(200)
   object1.update()
   object2.update()
-  /*
   if (object1.vector.x <= object2.vector.x + object2.size){
-    var pBefore = momentumBefore()
-    object1.velocity.x = elasticCollisionSpeed(object1.size, object2.size, object1.velocity.x, object2.velocity.x)
-    object2.velocity.x = elasticCollisionSpeed(object2.size, object1.size, object2.velocity.x, object1.velocity.x)
-    console.log("object1 velocity = " + object1.velocity.x, "object2 velocity = " + object2.velocity.x)
-  }
-  if (object1.vector.x <= object2.vector.x - object2.size && object2.vector.x < 0){
-    var pBefore = momentumBefore()
-    object1.velocity.x = elasticCollisionSpeed(object1.size, object2.size, object1.velocity.x, object2.velocity.x)
-    object2.velocity.x = elasticCollisionSpeed(object2.size, object1.size, object2.velocity.x, object1.velocity.x)
-    console.log("object1 velocity = " + object1.velocity.x, "object2 velocity = " + object2.velocity.x)
-  }*/
-  if (object1.vector.x <= object2.vector.x + object2.size){
-    object1.velocity.x = elasticCollisionSpeed(object1.size, object2.size, object1.velocity.x, object2.velocity.x).v1
-    object2.velocity.x = elasticCollisionSpeed(object1.size, object2.size, object1.velocity.x, object2.velocity.x).v2
+    var velocities = elasticCollisionSpeed(object1.size, object2.size, object1.velocity.x, object2.velocity.x)
+    object1.velocity.x = velocities.v1
+    object2.velocity.x = velocities.v2
   }
 
   if (object1.vector.x <= object2.vector.x - object2.size && object2.vector.x < 0){
-    object1.velocity.x = elasticCollisionSpeed(object1.size, object2.size, object1.velocity.x, object2.velocity.x).v1
-    object2.velocity.x = elasticCollisionSpeed(object1.size, object2.size, object1.velocity.x, object2.velocity.x).v2
+    var velocities = elasticCollisionSpeed(object1.size, object2.size, object1.velocity.x, object2.velocity.x)
+    object1.velocity.x = velocities.v1
+    object2.velocity.x = velocities.v2
   }
   object2.display()
   object1.display()
-  text("object1 velocity = " + object1.velocity.x + "\nobject2 velocity = " + object2.velocity.x + "\nMomentum = " + momentumBefore()[1] + "\nKE = " + kineticEnergy().x, 0, 300 )  
- 
-  
+  text("object1 velocity = " + object1.velocity.x + "\nobject2 velocity = " + object2.velocity.x + "\nMomentum = " + momentumBefore()[1] + "\nKE = " + kineticEnergy(), 0, 300 ) 
 }
